ba5ee7c254102159aa3a44a86968c3a87579b19c
supercrashiha <shantanetka@gmail.com>
2024-02-16 23:49:27 +0100

    newest version

--- js/script.js
+++ js/script.js
@@ -34,23 +34,27 @@ const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
-    e.preventDefault();
     const href = link.getAttribute("href");
 
-    // Scroll back to top
-    if (href === "#")
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
+    // Only prevent default if href is an internal link
+    if (href.startsWith("#")) {
+      e.preventDefault(); // Prevent default only for internal links
 
-    // Scroll to other links
-    if (href !== "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth" });
+      // Scroll back to top
+      if (href === "#")
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+
+      // Scroll to other links
+      if (href !== "#") {
+        const sectionEl = document.querySelector(href);
+        sectionEl.scrollIntoView({ behavior: "smooth" });
+      }
     }
 
-    // Close mobile naviagtion
+    // Close mobile navigation for all links
     if (link.classList.contains("main-nav-link"))
       headerEl.classList.toggle("nav-open");
   });
